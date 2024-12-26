af4324489e53ca77ad2f5779df743a2be1fd16d9
Anastasiia Derevianko <aderevianko@proton.me>
2024-12-26 16:51:52 +0200

    delete tamplate data (#3)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,9 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/tasks')
-      .then(response => response.json())
-      .then(json => setData(json))
-      .catch(error => console.error(error));
-  }, []);
-  console.log(data)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>DATA {data}</h1>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>lumio</h1>
     </>
   )
 }
