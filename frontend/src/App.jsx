832fb7d3b6f19717f1aed1c67a03313fc2495312
Anastasiia Derevianko <aderevianko@proton.me>
2024-12-27 14:50:42 +0200

    add side menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,40 @@
 import './App.css'
-import NavBar from './main/NavBar.jsx'
+import SideBar from './main/sidebar/SideBar.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import { Team, Home } from "./main/sidebar/routes.jsx";
+
+const AppLayout = () => (
+  <>
+    <SideBar />
+    <Outlet />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "team",
+        element: <Team />,
+      }
+    ],
+  },
+]);
 
 function App() {
   return (
-    <>
-      <NavBar>lumio</NavBar>
+    <><RouterProvider router={router}>
+    </RouterProvider>
     </>
   )
 }
---
+++ frontend/src/main/sidebar/routes.jsx
@@ -0,0 +1,15 @@
+export function Home() {
+    return (
+        <div className="home">
+            <h1>Home</h1>
+        </div>
+    );
+}
+export function Team() {
+    return (
+        <div className="team">
+            <h1>Team</h1>
+        </div>
+    );
+}
+
