a86bd7d4ae870140f89bb1d7b794543243b30f86
Anastasiia Derevianko <aderevianko@proton.me>
2025-02-17 22:53:43 +0200

    format time fields

--- core/tasks/serializers.py
+++ core/tasks/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from .models import Task
+from django.utils import timezone
 
 class TasksSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+    modified_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
     class Meta:
         model = Task
         fields = '__all__'
     
+
 class DeleteTasksSerializer(serializers.Serializer):
     ids = serializers.ListField(
         child=serializers.UUIDField(),
