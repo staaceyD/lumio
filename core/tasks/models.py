76cdc51c2a92b08aab4af08cf32f68536b94a49b
Anastasiia Derevianko <aderevianko@proton.me>
2025-08-25 18:48:41 +0300

    add api for labels

--- core/tasks/models.py
+++ core/tasks/models.py
@@ -1,6 +1,22 @@
-from django.db import models
 import uuid
+
 from django.contrib.auth.models import User
+from django.db import models
+
+
+class Label(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=7, blank=True, null=True)  # HEX code optional
+    # TODO enable when auth added
+    # models.ForeignKey(User, on_delete=models.CASCADE, related_name="label")
+
+    # class Meta:
+    #     unique_together = ("name", "user")  # Each user can have labels with unique names
+
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -9,11 +25,11 @@ class Task(models.Model):
     note = models.TextField(null=True, blank=True)
     status = models.CharField(max_length=100, default="Not started")
     priority = models.CharField(max_length=100, null=True, blank=True)
-    label = models.CharField(max_length=100, null=True, blank=True)
+    labels = models.ManyToManyField(Label, blank=True, related_name="task")
     # TODO enable when auth added
     # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="task")
     due_date = models.DateField(null=True, blank=True)
-    minutes_spent = models.IntegerField(null=True, blank=True) 
+    minutes_spent = models.IntegerField(null=True, blank=True)
     position = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
@@ -22,10 +38,10 @@ class Task(models.Model):
         # # TODO enable when auth added
         # tasks = Task.objects.filter(author=User)
         tasks = Task.objects.all()
-        
+
         # handle position auto increment
         if tasks.exists() and self._state.adding:
-            last_task = tasks.latest('position')
+            last_task = tasks.latest("position")
             self.position = int(last_task.position) + 1
         super().save(*args, **kwargs)
 
--- core/tasks/views.py
+++ core/tasks/views.py
@@ -1,17 +1,19 @@
-from .models import Task
+from django.shortcuts import get_list_or_404, get_object_or_404
 from drf_spectacular.utils import extend_schema, extend_schema_view
-from .serializers import TasksSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404, get_list_or_404
+
+from .models import Label, Task
+from .serializers import LabelsSerializer, TasksSerializer
+
 
 @extend_schema_view(
     get=extend_schema(
         description="Returns a list of Task model instances.", responses=TasksSerializer
     ),
     post=extend_schema(
-        description="Creates a new post by authenticated user",
+        description="Creates a new task by user",
         request=TasksSerializer,
         responses=TasksSerializer,
     ),
@@ -35,6 +37,7 @@ def tasks(request):
         serializer = TasksSerializer(tasks, many=True)
         return Response(serializer.data)
 
+
 @extend_schema_view(
     delete=extend_schema(
         description="Deletes multiple Task model instances.",
@@ -43,7 +46,7 @@ def tasks(request):
 )
 @api_view(["DELETE"])
 def delete_multiple_tasks(request, task_ids):
-    ids = task_ids.split(',')
+    ids = task_ids.split(",")
     tasks = get_list_or_404(Task, id__in=ids)
     deleted_count, _ = Task.objects.filter(id__in=ids).delete()
 
@@ -51,6 +54,7 @@ def delete_multiple_tasks(request, task_ids):
         {"message": f"{deleted_count} tasks deleted successfully"},
         status=status.HTTP_204_NO_CONTENT,
     )
+
 
 @extend_schema_view(
     get=extend_schema(
@@ -78,9 +82,68 @@ def task(request, task_id):
         return Response(status=status.HTTP_204_NO_CONTENT)
     if request.method == "PATCH":
         serializer = TasksSerializer(task, data=request.data, partial=True)
-        
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@extend_schema_view(
+    get=extend_schema(
+        description="Returns a list of labels created by user.",
+        responses=TasksSerializer,
+    ),
+    post=extend_schema(
+        description="Creates a new label by user",
+        request=TasksSerializer,
+        responses=TasksSerializer,
+    ),
+)
+@api_view(["GET", "POST"])
+def labels(request):
+    if request.method == "POST":
+        # TODO enable when auth is added
+        # request.data["author"] = request.user.pk
+        serializer = LabelsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "GET":
+        # TODO enable when auth added
+        # user = request.user
+        # tasks = Task.objects.filter(author=user)
+        labels = Label.objects.all()
+        serializer = LabelsSerializer(labels, many=True)
+        return Response(serializer.data)
+
+
+@extend_schema_view(
+    patch=extend_schema(
+        description="Updates a single label.",
+        request=TasksSerializer,
+        responses=TasksSerializer,
+    ),
+    delete=extend_schema(
+        description="Deletes a single label instance.",
+        responses=TasksSerializer,
+    ),
+)
+@api_view(["PATCH", "DELETE"])
+def label(request, label_id):
+    label = get_object_or_404(Label, id=label_id)
+
+    if request.method == "DELETE":
+        label.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    if request.method == "PATCH":
+        serializer = LabelsSerializer(label, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
